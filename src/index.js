aeccc0bb500b9d528edff2f84b44bfd772c1d4c7
Kim Neunert <k9ert@gmx.de>
2023-01-22 22:34:43 +0100

    adding Apollo, fix versions again and test query

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,23 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+const client = new ApolloClient({
+
+  uri: 'http://0.0.0.0:8000/graphql',
+
+  cache: new InMemoryCache(),
+
+});
+
+client.query({
+    query: gql`
+      query Bookmarks {
+        bookmarks {
+          url
+          desc
+        }
+      }
+    `,
+    }).then((result) => console.log(result));
