d318600e5594aa91393875f6b5586b29235a1c3a
Kim Neunert <k9ert@gmx.de>
2023-02-04 20:43:08 +0100

    make tag editable

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,14 +1,56 @@
 import { Link } from "react-router-dom";
 
+import React, { useState } from 'react';
+
+
 const Tag = (props) => {
-    const tagString = typeof props.tag === 'string' ? props.tag : props.tag.name;
-    return (
-      <div className='rounded-lg bg-gray-500 py-0 px-3 m-1'>
-        <Link to={"/tag/"+tagString}>
-            <span className='text-sm'>{tagString}</span>
+  const [editing, setEditing] = useState(false);
+  const [tagString, setTagString] = useState(
+    typeof props.tag === 'string' ? props.tag : props.tag.name
+  );
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleSave = () => {
+    setEditing(false);
+    // Save the updated tag here
+  };
+
+  return (
+    <div className="rounded-lg bg-gray-500 py-0 px-3 m-1 flex items-center">
+        {editing ? (
+        <input
+            className="py-2 px-3 rounded-lg text-sm bg-gray-100 outline-none focus:shadow-outline w-full"
+            type="text"
+            value={tagString}
+            onChange={(e) => setTagString(e.target.value)}
+        />
+        ) : (
+        <Link to={"/tag/" + tagString} className="text-sm">
+            <span className="text-white">{tagString}</span>
         </Link>
-      </div>
-    )
-  }
+        )}
+        {editing ? (
+        <button
+            className="text-white text-sm bg-green-500 p-1 rounded-sm mr-2"
+            onClick={handleSave}
+        >
+            Save
+        </button>
+        ) : (
+        <button
+            className="text-sm  text-white bg-blue-500 ml-1 p-1 rounded-sm mr-0"
+            onClick={handleEdit}
+        >
+            Edit
+        </button>
+        )}
+    </div>
+  );
+};
+
+export default Tag;
 
 export { Tag }
