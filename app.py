b5cee0508866e2187d24e40d16598c1f012e3928
Kim Neunert <k9ert@gmx.de>
2023-02-05 17:45:38 +0100

    use a database and initially fill it with the json data

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from strawberry.flask.views import GraphQLView
 from flask_cors import CORS
 from schema import *
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000"}})
@@ -10,6 +11,19 @@ app.add_url_rule(
     "/graphql",
     view_func=GraphQLView.as_view("graphql_view", schema=schema),
 )
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.db = SQLAlchemy()
+app.db.init_app(app)
+with app.app_context():
+    from db import Tag, Bookmark, fill_database
+    
+    app.db.create_all()
+    if not Bookmark.query.all():
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+        fill_database(app.db.session, data)
+
 
 
 if __name__ == "__main__":
---
+++ db.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from typing import List
+import json
+from flask_sqlalchemy import SQLAlchemy
+from flask import current_app as app
+
+bookmark_tags = app.db.Table('bookmark_tags',
+    app.db.Column('bookmark_id', Integer, ForeignKey('bookmark.id'), primary_key=True),
+    app.db.Column('tag_id', Integer, ForeignKey('tag.id'), primary_key=True)
+)
+
+class Tag(app.db.Model):
+    __tablename__ = 'tag'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    bookmark_id = Column(Integer, ForeignKey("bookmark.id"))
+    bookmarks = relationship("Bookmark", secondary=bookmark_tags, backref="tags")
+
+class Bookmark(app.db.Model):
+    __tablename__ = 'bookmark'
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    desc = Column(String)
+    readlater = Column(String)
+    annotations = Column(String)
+    comments = Column(String)
+    user = Column(String)
+    shared = Column(String)
+    created_at = Column(String)
+    updated_at = Column(String)
+    title = Column(String)
+
+
+def fill_database(session, data):
+    for bookmark_data in data:
+        tag_list = bookmark_data['tags'].split(",")
+        tags = []
+        for name in tag_list:
+            tag = session.query(Tag).filter(Tag.name == name).first()
+            if not tag:
+                tag = Tag(name=name)
+            tags.append(tag)
+
+        bookmark = Bookmark(
+            url=bookmark_data['url'],
+            desc=bookmark_data['desc'],
+            readlater=bookmark_data['readlater'],
+            annotations=json.dumps(bookmark_data['annotations']),
+            comments=json.dumps(bookmark_data['comments']),
+            user=bookmark_data['user'],
+            shared=bookmark_data['shared'],
+            created_at=bookmark_data['created_at'],
+            updated_at=bookmark_data['updated_at'],
+            title=bookmark_data['title'],
+            tags=tags
+        )
+        session.add(bookmark)
+    session.commit()
