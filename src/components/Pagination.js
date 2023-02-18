cd3394f3be4bc22ff120bdac7a8365874704e1b8
Kim Neunert <k9ert@gmx.de>
2023-02-18 17:22:58 +0100

    fixing Pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,22 +1,26 @@
 import React from "react";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  
+  //returns an array like (4,8): [4,5,6,7,8]
   const range = (start, end) =>
     [...Array(end - start + 1)].map((_, i) => start + i);
+  
 
   const handleClick = (page) => {
     onPageChange(page);
   };
 
   const renderPageNumbers = () => {
-    const pageNumbers = range(1, totalPages);
+    const pageNumbers = range(0, totalPages-1);
 
     return pageNumbers.map((page) => (
+      
       <div key={page}>
         <a
           href="#"
           className={`${
-            currentPage === page ? "bg-gray-500 text-white" : "bg-white"
+            currentPage == page ? "bg-gray-500 text-white" : "bg-white"
           } border-gray-500 border px-4 py-2 mx-1 rounded-lg hover:bg-gray-200`}
           onClick={() => handleClick(page)}
         >
@@ -33,12 +37,12 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         <a
           href="#"
           className={`${
-            currentPage === 1
-              ? "bg-gray-200 cursor-not-allowed"
+            currentPage === 0
+              ? "bg-gray-100 cursor-not-allowed"
               : "bg-gray-200 hover:bg-gray-300"
           } text-gray-600 hover:text-gray-800 rounded-l-lg py-2 px-4`}
-          onClick={() => handleClick(Math.floor(currentPage - 1,0))}
-          disabled={currentPage === 1}
+          onClick={() => currentPage === 0 || handleClick(Math.floor(currentPage - 1,0))}
+          disabled={currentPage === 0}
         >
           Previous
         </a>
@@ -50,12 +54,11 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         <a
           href="#"
           className={`${
-            currentPage === totalPages
-              ? "bg-gray-200 cursor-not-allowed"
-              : "bg-gray-200 hover:bg-gray-300"
+            currentPage === totalPages-1
+              ? "bg-gray-100 cursor-not-allowed"
+              : "bg-gray-300 hover:bg-gray-400"
           } text-gray-600 hover:text-gray-800 rounded-r-lg py-2 px-4`}
-          onClick={() => handleClick(currentPage + 1)}
-          disabled={currentPage === totalPages}
+          onClick={(event) => { currentPage === totalPages-1 || handleClick(currentPage + 1) }}
         >
           Next
         </a>
