b0ad45ba0c496a48b46f4b367edd08ae539ff0f8
Kim Neunert <k9ert@gmx.de>
2023-02-19 22:38:02 +0100

    refactoring Pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,31 +1,118 @@
 import React from "react";
 import { useState } from 'react';
 
-const NoPageElement = () => {
-  return (
+const PageNavElement = ({selected, hoverEffect, disabled, onClick, argument, children}) => {
+  return(
     <div
       key="prev-ellipsis"
-      className="border-gray-500 border px-4 py-1 mx-1 rounded-lg hover:bg-gray-200 cursor-not-allowed"
-      onClick={(e) => e.preventDefault()}
+      className={
+        `
+          ${
+            disabled
+              ? "bg-gray-100 cursor-not-allowed"
+              : "bg-gray-200 hover:bg-gray-300"
+          }
+          ${selected ? "bg-gray-500 text-white" : "bg-white"} 
+          border-gray-500 border py-2 px-4 py-1 mx-1 rounded-lg 
+          ${ hoverEffect ? "hover:bg-gray-200":""}
+      `}
+      onClick={onClick}
     >
-      ...
+      {children}
     </div>
+  )
+}
+
+const NoPageElement = () => {
+  return (
+    <PageNavElement selected={false} hoverEffect={false} onClick={(e) => e.preventDefault()}>
+      ...
+    </PageNavElement>
   )
 
 }
 
 const PageElement = ({page, currentPage, handleClick}) => {
-  return (  <div
-      key={page}
-      className={`${
-        currentPage === page ? "bg-gray-500 text-white" : "bg-white"
-      } border-gray-500 border px-4 py-1 mx-1 rounded-lg hover:bg-gray-200`}
-      onClick={() => handleClick(page)}
-    >
+  return (  
+    <PageNavElement selected={currentPage === page } hoverEffect={true} onClick={() => handleClick(page)}>
       {page}
-    </div>
+    </PageNavElement>
   )
 }
+
+
+const PageNumbers = ({currentPage, totalPages, handleClick }) => {
+  const start = currentPage - 1 < 0 ? 0 : currentPage-1
+  const end = currentPage + 1 > totalPages ? totalPages : currentPage +1
+  const pageNumbers = [...Array(end - start + 1)].map((_, i) => start + i);
+
+  const renderPages = () => {
+    const pages = [];
+    /* Previous button */
+    pages.push(
+      <PageNavElement selected={false} hoverEffect={false} disabled={currentPage === 0} 
+        onClick={() => currentPage === 0 || handleClick(Math.floor(currentPage - 1,0))}>
+          Previous
+      </PageNavElement>
+    );
+    /* 0 Button and a seperator (if necessary */
+    if (currentPage >= 3) {
+      pages.push(
+        <PageElement page={0} currentPage={currentPage} handleClick={handleClick}/>
+      );
+      pages.push(
+        <NoPageElement/>
+      );
+    }
+
+    /* Three pagebutton (number or array elements) for individual pages */
+    pageNumbers.forEach((page) => {
+      pages.push(
+        <PageElement page={page} currentPage={currentPage} handleClick={handleClick}/>
+      );
+    });
+
+    /* seperator and maxPage Button (if necessary */
+    if (currentPage+2 < totalPages) {
+      pages.push(
+        <NoPageElement/>
+      );
+      pages.push(
+        <PageElement page={totalPages} currentPage={currentPage} handleClick={handleClick}/>
+      );
+    }
+    /* Next button */
+    pages.push(
+      <PageNavElement selected={false} hoverEffect={false} disabled={currentPage === totalPages-1} 
+      onClick={(event) => { currentPage === totalPages-1 || handleClick(currentPage + 1) }}>
+        Next
+      </PageNavElement>
+    );
+
+    return pages;
+  };
+
+  return <>{renderPages()}</>;
+};
+
+const ResultsPerPageDropdown = ({resultsPerPage, handleResultsPerPageChange}) => {
+  const options = [5, 10, 20, 50];
+  return (
+    <select
+      className="border-gray-500 border px-4 py-2 mx-1 rounded-lg"
+      value={resultsPerPage}
+      onChange={handleResultsPerPageChange}
+    >
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option} per page
+        </option>
+      ))}
+    </select>
+  );
+};
+
+
 
 const Pagination = ({ currentPage, totalPages, pageSize, onPageChange }) => {
   const [resultsPerPage, setResultsPerPage] = useState(pageSize);
@@ -40,102 +127,20 @@ const Pagination = ({ currentPage, totalPages, pageSize, onPageChange }) => {
     onPageChange(0, Number(event.target.value)); // Reset the current page to 0 when changing the results per page
   };
 
-  const PageNumbers = ({currentPage}) => {
-    console.log({ currentPage: Number(currentPage)})
-    const start = currentPage - 1 < 0 ? 0 : currentPage-1
-    const end = currentPage + 1 > totalPages ? totalPages : currentPage +1
-    console.log({start:start,end:end})
-    const pageNumbers = [...Array(end - start + 1)].map((_, i) => start + i);
-    console.log(pageNumbers)
-
-    const renderPages = () => {
-      const pages = [];
-
-      if (currentPage >= 3) {
-        pages.push(
-          <PageElement page={0} currentPage={currentPage} handleClick={handleClick}/>
-        );
-        pages.push(
-          <NoPageElement/>
-        );
-      }
-
-      pageNumbers.forEach((page) => {
-        pages.push(
-          <PageElement page={page} currentPage={currentPage} handleClick={handleClick}/>
-        );
-      });
-
-      if (currentPage+2 < totalPages) {
-        pages.push(
-          <NoPageElement/>
-        );
-        pages.push(
-          <PageElement page={totalPages} currentPage={currentPage} handleClick={handleClick}/>
-        );
-      }
-
-      return pages;
-    };
-
-    return <>{renderPages()}</>;
-  };
-
-  const renderResultsPerPageDropdown = () => {
-    const options = [5, 10, 20, 50];
-    return (
-      <select
-        className="border-gray-500 border px-4 py-2 mx-1 rounded-lg"
-        value={resultsPerPage}
-        onChange={handleResultsPerPageChange}
-      >
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option} per page
-          </option>
-        ))}
-      </select>
-    );
-  };
-
   return (
     <div className="flex justify-center my-4">
       <nav className="inline-flex shadow-lg">
-        {/* Previous button */}
-        <div
-          className={`${
-            currentPage === 0
-              ? "bg-gray-100 cursor-not-allowed"
-              : "bg-gray-200 hover:bg-gray-300"
-          } text-gray-600 hover:text-gray-800 rounded-l-lg py-2 px-4`}
-          onClick={() => currentPage === 0 || handleClick(Math.floor(currentPage - 1,0))}
-          disabled={currentPage === 0}
-        >
-          Previous
-        </div>
-
         {/* Pagination buttons */}
         <div className="flex">
-          <PageNumbers currentPage={currentPage}/>
+          <PageNumbers currentPage={currentPage} totalPages={totalPages} handleClick={handleClick}/>
         </div>
 
-        {/* Next button */}
-        <div
-          className={`${
-            currentPage === totalPages-1
-              ? "bg-gray-100 cursor-not-allowed"
-              : "bg-gray-300 hover:bg-gray-400"
-          } text-gray-600 hover:text-gray-800 rounded-r-lg py-2 px-4`}
-          onClick={(event) => { currentPage === totalPages-1 || handleClick(currentPage + 1) }}
-        >
-          Next
-        </div>
         {/* Results per page dropdown */}
         <div className="flex items-center mx-2">
           <label htmlFor="results-per-page" className="mr-2">
             Results per page:
           </label>
-          {renderResultsPerPageDropdown()}
+          <ResultsPerPageDropdown resultsPerPage={resultsPerPage} handleResultsPerPageChange={handleResultsPerPageChange}/>
         </div>
       </nav>
     </div>
